f6b49daaca706ef92a2336aca3e65c2a6955714b
Gera <german.titoww@gmail.com>
2023-02-13 06:46:19 +0300

    add examples004_div

--- examples001_helloconsole/examples004_div/Program.cs
+++ examples001_helloconsole/examples004_div/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numberA = new Random ().Next(1, 10); // 1 2 3 4....9
+int numberB = new Random ().Next(1, 10);
+int result =numberA +numberB;
+Console.WriteLine(result);
